c0f899f4b9eaa21dd86b1fbb170c9fe6d83a753a
simaokane <simadrien@gmail.com>
2022-10-31 04:22:35 +0100

    Insertion du produit et ses détails dans la page produit

--- front/html/JS/product.js
+++ front/html/JS/product.js
@@ -1,53 +1,71 @@
 
 //Recupération de l'id avec les paramètres de l'url
 const produitId = (new URL(document.location)).searchParams.get("id");
-console.log(produitId);
+console.log({produitId});
 
-//Récupération des sélecteurs css et des id du HTML pour après
-let picture     = document.querySelector(".item__img"); // on récupére le selecteur css pour pouvoir mettre l'image après
-let title       = document.getElementById("title"); // on récupéré l'id title du document HTML
-let price       = document.getElementById("price"); // on récupére l'id price du document HTML
-let description = document.getElementById("description"); // on récupére l'id description du document HTML
-let colorsArray = document.getElementById("colors"); // on récupére l'id description du document HTML
 
 // fonction pour récuperer les données de l'api avec l'id du produit
-let product;
-const fetchProduct = async() => {
-    await fetch(`http://localhost:3000/api/products/${produitId}`) // on va chercher l'API avec la methode fetch et on ajoute notre variable qui contient l'id
-    .then((res) => res.json()
-    .then(json => product = json));  
-    };
-
-fetchProduct(); // On appelle la fonction précédente pour récupérer les données de l'API
-
+ fetch(`http://localhost:3000/api/products/${produitId}`) // on va chercher l'API avec la methode fetch et on ajoute notre variable qui contient l'id
+    .then((response) => response.json())
+    .then((res) => productDisplay(res)) 
+    
 // fonction pour lier les élements HTML que l'on va créer avec les données de l'api
-const displayProduct = async() => { 
-    await fetchProduct(); 
+function productDisplay(sofa) {
+    const {altTxt, colors, description, imageUrl, name, price, _id} = sofa
+    createImage(imageUrl, altTxt)
+    createTitle(name)
+    createPrice(price)
+    createDescription(description)
+    createColors(colors) 
+}
+//Fonction pour ajouter les balises img pour y mettre des images
+function createImage(imageUrl, altTxt) {
+    const image = document.createElement("img")
+    image.src = imageUrl
+    image.alt = altTxt
+    const parent = document.querySelector(".item__img")
+    if(parent != null) parent.appendChild(image)
+}
 
-        // on ajoute les balises img pour y mettre les images
-        let image = document.createElement("img");
-        image.setAttribute('src', product.imageUrl);
-        image.setAttribute('alt', product.altTxt);
-        picture.appendChild(image);
+//Ajout du nom
+function createTitle(name) {
+    const h1 = document.querySelector("#title")
+    if(h1 != null) h1.textContent = name
+}
 
-        // on ajoute le nom
-        title.innerHTML = product.name;
+//Ajout du prix
+function createPrice(price) {
+    const span = document.querySelector("#price")
+    if(span != null) span.textContent = price
 
-        // on ajoute les prix
-        price.innerHTML = product.price;
+}
 
-        // on ajout la description
-        description.innerHTML = product.description;
+//Ajout de la description
+function createDescription(description) {
+    const p = document.querySelector("#description")
+    if(p != null) p.textContent = description
+}
 
-        // Couleurs du tableau colors avec une boucle for en créant le HTML
-        for (let i=0; i < product.colors.length; i++) {
+//Ajout des couleurs du tableau colors avec une boucle forEach
+function createColors(colors) {
+    const select = document.querySelector("#colors")
+    if(select != null) {
+        colors.forEach(color => {
+            const option = document.createElement("option")
+            option.value = color
+            option.textContent = color
+            select.appendChild(option)
+                     
+        });
+    }
 
-          let color = document.createElement("option");
-          color.setAttribute('value', product.colors[i]);
-          color.innerHTML = product.colors[i];
-          colorsArray.appendChild(color);
-        }
+}
 
-    fetchProduct(product); // Déclaration de la fonction et recupération des parametres 
-};
-displayProduct();
+
+
+
+
+
+
+
+
