45eb425ea7dc53b2c06ce1082f1435e4cc874f35
simaokane <simadrien@gmail.com>
2022-12-22 19:16:49 +0100

    Gestion de la modification et de la suppression  de produits dans le panier

--- front/html/JS/cart.js
+++ front/html/JS/cart.js
@@ -1,79 +1,297 @@
-// <!-- <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
-//               <div class="cart__item__img">
-//                 <img src="../images/product01.jpg" alt="Photographie d'un canapé">
-//               </div>
-//               <div class="cart__item__content">
-//                 <div class="cart__item__content__description">
-//                   <h2>Nom du produit</h2>
-//                   <p>Vert</p>
-//                   <p>42,00 €</p>
-//                 </div>
-//                 <div class="cart__item__content__settings">
-//                   <div class="cart__item__content__settings__quantity">
-//                     <p>Qté : </p>
-//                     <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-//                   </div>
-//                   <div class="cart__item__content__settings__delete">
-//                     <p class="deleteItem">Supprimer</p>
-//                   </div>
-//                 </div>
-//               </div>
-//             </article> -->
+//Recupération depuis le localstorage
+let panier = JSON.parse(localStorage.getItem('panier'));
 
-//Retirer un produit du panier
-function removePanier(product) {
-  let panier = getPanier();
-  panier = panier.filter((p = p.id != product.id)); //Filter permet de pouvoir supprimer un element
-  savePanier(panier);
-}
+let arrayQuantity = [];
+let arrayPrix = [];
+let arrayProduitId = [];
 
-//Changer la quantité(retirer la quantité)
-function changeQuantity(product, quantity) {
-  let panier = getPanier(); //On recupere le panier complet
-  let foundProduct = panier.find((p) => p.id == product.id);
-  if (foundProduct != undefined) {
-    foundProduct.quantity += quantity;
-    if (foundProduct.quantity <= 0) {
-      removePanier(foundProduct);
-    } else {
-      savePanier(panier); //On l'enregistre que si le produit n'a pas été supprimé
-    }
+/* afficher le contenu de notre panier */
+
+if (panier != null && panier.length != 0) {
+  for (let i = 0; i < panier.length; i++) {
+    const lienkProduits = fetch(
+      'http://localhost:3000/api/products/' + `${panier[i].id}`
+    );
+    lienkProduits.then(async function (res) {
+      try {
+        let produitRecuperes = await res.json();
+
+        //Création de la fonction afficher
+        function afficherProduits() {
+          //Création et insertion des éléments
+          const cart__items = document.querySelector('#cart__items');
+          const article = document.createElement('article');
+          article.className = 'cart__item';
+          article.dataset.id = panier[i].id;
+          article.dataset.color = panier[i].color;
+
+          cart__items.appendChild(article);
+
+          //--------------//
+          const divItemImg = document.createElement('div');
+          divItemImg.className = 'cart__item__img';
+          const image = document.createElement('img');
+          image.src = produitRecuperes.imageUrl;
+          image.alt = produitRecuperes.altTxt;
+
+          article.appendChild(divItemImg);
+          divItemImg.appendChild(image);
+
+          //---------------------------------//
+          const divItemContent = document.createElement('div');
+          divItemContent.className = 'cart__item__content';
+
+          article.appendChild(divItemContent);
+
+          //------------------------------//
+          const divDescrip = document.createElement('div');
+          divDescrip.className = 'cart__item__content__description';
+          const nomProduit = document.createElement('h2');
+          nomProduit.textContent = produitRecuperes.name;
+          const colorProduit = document.createElement('p');
+          colorProduit.textContent = panier[i].color;
+          const prixProduit = document.createElement('p');
+          prixProduit.textContent = Number(produitRecuperes.price) + ' €';
+
+          divItemContent.appendChild(divDescrip);
+          divDescrip.appendChild(nomProduit);
+          divDescrip.appendChild(colorProduit);
+          divDescrip.appendChild(prixProduit);
+
+          //-----------------------------------//
+          const divItemSetting = document.createElement('div');
+          divItemSetting.className = 'cart__item__content__settings';
+
+          divItemContent.appendChild(divItemSetting);
+
+          //--------------------------------------
+          const divItemQuantity = document.createElement('div');
+          divItemQuantity.className = 'cart__item__content__settings__quantity';
+          const qte = document.createElement('p');
+          qte.textContent = 'Qté : ';
+          const setInput = document.createElement('input');
+          setInput.type = 'number';
+          setInput.className = 'itemQuantity';
+          setInput.name = 'itemQuantity';
+          setInput.min = '1';
+          setInput.max = '100';
+          setInput.value = panier[i].quantity;
+          setInput.textContent = panier[i].quantity;
+
+          divItemSetting.appendChild(divItemQuantity);
+          divItemQuantity.appendChild(qte);
+          divItemQuantity.appendChild(setInput);
+
+          //--------------------------------------
+          const divItemSettingDelete = document.createElement('div');
+          divItemSettingDelete.className =
+            'cart__item__content__settings__delete';
+          divItemContent.appendChild(divItemSettingDelete);
+
+          const supprimer = document.createElement('p');
+          supprimer.className = 'deleteItem';
+          supprimer.textContent = 'Supprimer';
+          divItemSettingDelete.appendChild(supprimer);
+
+          //Modification de la quantité des articles disponibles dans le panier
+          function modifierQuantité() {
+            setInput.addEventListener('change', modificationQuantite);
+
+            function modificationQuantite() {
+              if (parseInt(setInput.value, 10) < 1) {
+                alert(
+                  ' Veuillez renseigner la quantité de produits souhaité, au minimum un article'
+                );
+              } else if (parseInt(setInput.value, 10) > 100) {
+                alert(
+                  ' votre commande depasse la limite autorisée, qui est de 100 articles '
+                );
+                setInput.value = '100';
+              } else {
+                let panier = JSON.parse(localStorage.getItem('panier'));
+                panier[i].quantity = parseInt(setInput.value, 10);
+                localStorage.setItem('panier', JSON.stringify(panier));
+                window.location.reload();
+              }
+            }
+          }
+
+          //Fonction supprimer
+
+          function supprimerProduit() {
+            supprimer.addEventListener('click', actualiserLePanier);
+
+            function actualiserLePanier() {
+              let panier = JSON.parse(localStorage.getItem('panier'));
+              panier.splice(i, 1);
+              localStorage.setItem('panier', JSON.stringify(panier));
+              window.location.reload();
+            }
+          }
+          /** affichage du total article */
+          function afficherTotalArticles() {
+            arrayQuantity.push(parseInt(panier[i].quantity));
+            const totalArtiles = arrayQuantity.reduce(
+              (val1, val2) => val1 + val2,
+              0
+            );
+            const totalQuantity = document.querySelector('#totalQuantity');
+            totalQuantity.textContent = totalArtiles;
+          }
+
+          /** affichage du prix total*/
+          function afficherPrixTotal() {
+            arrayPrix.push(panier[i].quantity * produitRecuperes.price);
+            const prixTotal = arrayPrix.reduce((val1, val2) => val1 + val2, 0);
+            const totalPrice = document.querySelector('#totalPrice');
+            totalPrice.textContent = prixTotal;
+          }
+
+          /* execution des fonctions */
+          //--- modifier la quantité du produit dans le panier
+          modifierQuantité();
+          //-- supprimer un promer du panier
+          supprimerProduit();
+          //-- afficher le total des articles diponibles dans le panier
+          afficherTotalArticles();
+          //-- afficher le prix total des artiles
+          afficherPrixTotal();
+          arrayProduitId.push(cart[i].id);
+        }
+
+        /* affichons maintent les produits dans le panier */
+        afficherProduits();
+      } catch (err) {
+        console.log(err);
+      }
+    });
   }
+} else {
+  const cart__items = document.querySelector('#cart__items');
+  cart__items.textContent = "Vous n'avez d'article dans votre panier";
+  const totalQuantity = document.querySelector('#totalQuantity');
+  totalQuantity.textContent = '0';
+  const totalPrice = document.querySelector('#totalPrice');
+  totalPrice.textContent = '0';
 }
 
-//Calcul de la quantité
-function getNumberProduct() {
-  //A partir du panier , retourner la qté de product qui se trouve dans le panier
-  let panier = getPanier(); //On recupere le panier complet
-  let number = 0;
-  for (let product of panier) {
-    number += product.quantity;
-  }
-  return number;
-}
+// /** du panier au passage à la commande */
+// // on declare nos varibles
+// const firstName = document.querySelector('#firstName');
+// const lastName = document.querySelector('#lastName');
+// const address = document.querySelector('#address');
+// const city = document.querySelector('#city');
+// const email = document.querySelector('#email');
 
-//Calcul du prix total
-function getTotalPrice() {
-  let panier = getPanier();
-  let total = 0;
-  for (let product of panier) {
-    total += product.price * product.quantity;
-  }
-  return total;
-}
+// let clientForm = [firstName, lastName, address, city, email];
 
-//Fonction de sauvegarde dans le localstorage
-function savePanier(panier) {
-  localStorage.setItem('panier', JSON.stringify(panier));
-  alert('Le produit a bien été enregistré');
-}
+// const firstNameErrorMsg = document.querySelector('#firstNameErrorMsg');
+// const lastNameErrorMsg = document.querySelector('#lastNameErrorMsg');
+// const addressErrorMsg = document.querySelector('#addressErrorMsg');
+// const cityErrorMsg = document.querySelector('#cityErrorMsg');
+// const emailErrorMsg = document.querySelector('#emailErrorMsg');
 
-//Recuperation du panier
-function getPanier() {
-  let panier = localStorage.getItem('panier');
-  if (panier == null) {
-    return [];
-  } else {
-    return JSON.parse(panier);
-  }
-}
+// let errorMsg = [
+//   firstNameErrorMsg,
+//   lastNameErrorMsg,
+//   addressErrorMsg,
+//   cityErrorMsg,
+//   emailErrorMsg,
+// ];
+
+// let textRegExp = new RegExp("^[A-Za-zÀ-ÿ '-]+[^0-9_!¿/+=@#$%&(){}|~<>;:]$");
+// let addressRegExp = new RegExp("^[0-9a-zA-ZÀ-ÿ',-. ]+$");
+// let emailRegExp = new RegExp('[A-Za-z0-9]+@[A-Za-z.-]+[.]+[A-Za-z]{2,3}$');
+
+// function verificationInput() {
+//   let arrayTests = [
+//     textRegExp.test(firstName.value),
+//     textRegExp.test(lastName.value),
+//     addressRegExp.test(address.value),
+//     textRegExp.test(city.value),
+//     emailRegExp.test(email.value),
+//   ];
+
+//   /* Signaler les champs valides ou erronées */
+
+//   for (let i = 0; i < arrayTests.length; i++) {
+//     let validInput = 'solid green';
+//     let invalidInput = 'solid red';
+
+//     if (arrayTests[i] == true) {
+//       clientForm[i].style.border = validInput;
+//       errorMsg[i].textContent = '';
+//     } else {
+//       clientForm[i].style.border = invalidInput;
+//       errorMsg[i].textContent = 'Erreur de saisie';
+//     }
+//   }
+// }
+
+// /* Vérifier si le formulaire est correctement rempli */
+
+// function validatationForm() {
+//   if (
+//     textRegExp.test(firstName.value) == true &&
+//     textRegExp.test(lastName.value) == true &&
+//     addressRegExp.test(address.value) == true &&
+//     textRegExp.test(city.value) == true &&
+//     emailRegExp.test(email.value) == true
+//   ) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// /* action commander*/
+
+// function saisiClient() {
+//   const orderClick = document.querySelector('#order');
+//   orderClick.addEventListener('click', sendOrder);
+
+//   function sendOrder(submit) {
+//     submit.preventDefault();
+
+//     if (cart.length == 0) {
+//       alert("Vous n'avez aucun article dans le panier");
+//     } else {
+//       verificationInput();
+//       validatationForm();
+
+//       /* Valider la commande */
+
+//       if (validatationForm() == true) {
+//         const contact = {
+//           firstName: firstName.value,
+//           lastName: lastName.value,
+//           address: address.value,
+//           city: city.value,
+//           email: email.value,
+//         };
+
+//         const data = {
+//           products: arrayProduitId,
+//           contact: contact,
+//         };
+
+//         const orderPost = fetch('http://localhost:3000/api/products/order', {
+//           method: 'POST',
+//           headers: {
+//             Accept: 'application/json',
+//             'Content-Type': 'application/json',
+//           },
+//           body: JSON.stringify(data),
+//         });
+
+//         orderPost.then(async function (res) {
+//           const order = await res.json();
+//           window.location.href = `../html/confirmation.html?id=${order.orderId}`;
+//         });
+//       } else {
+//         alert('Veuillez vérifier le formulaire');
+//       }
+//     }
+//   }
+// }
+// saisiClient();
